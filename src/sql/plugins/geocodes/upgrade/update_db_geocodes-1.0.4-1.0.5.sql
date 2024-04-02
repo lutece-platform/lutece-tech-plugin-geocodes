82ebde59a9b89874289362632b7462ef038e3705
Félix Dupriez <felix.dupriez@accenture.com>
2024-04-02 16:30:56 +0200

    LUT-27634 : Rename value_min_complete for Paris 1er and Paris 2e

---
+++ src/sql/plugins/geocodes/upgrade/update_db_geocodes-1.0.4-1.0.5.sql
@@ -0,0 +1,2 @@
+update geocodes_city set value_min_complete = 'Paris 1er arrondissement' where code='75101';
+update geocodes_city set value_min_complete = 'Paris 2e arrondissement' where code='75102';
