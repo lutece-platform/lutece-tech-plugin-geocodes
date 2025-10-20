027a62d5394dc398341f248131805ce9e2786c8a
Norbert Le Garrec <norbert.legarrec2@paris.fr>
2025-10-20 12:08:39 +0200

    LUT-30981 : fix sql file create table

--- src/sql/plugins/geocodes/plugin/create_db_geocodes.sql
+++ src/sql/plugins/geocodes/plugin/create_db_geocodes.sql
@@ -47,4 +47,38 @@ CREATE INDEX IDX_VALUE_MIN_CITY on geocodes_city ( value_min ASC);
 CREATE INDEX IDX_VALUE_MIN_COMPLETE_CITY on geocodes_city ( value_min_complete ASC);
 
 
+DROP TABLE IF EXISTS geocodes_city_changes;
+CREATE TABLE geocodes_city_changes (
+	   code_country varchar(10) default '' NOT NULL,
+	   code varchar(10) default '' NOT NULL,
+	   code_zone varchar(10) default '' NOT NULL,
+	   value varchar(255) default '' NOT NULL,
+	   date_validity_start DATE default '1943-01-01',
+	   date_validity_end DATE default '2999-12-31',
+	   value_min varchar(255) default '' NOT NULL,
+	   value_min_complete varchar(255) default '' NOT NULL,
+	   deprecated SMALLINT DEFAULT 0,
+	   date_update DATE default '1943-01-01',
+	   author varchar(255) default '' NOT NULL,
+	   status varchar(50) DEFAULT '' NOT NULL,
+	   PRIMARY KEY (code, date_validity_start)
+);
 
+DROP TABLE IF EXISTS geocodes_country_changes;
+CREATE TABLE geocodes_country_changes (
+    id_country_history SERIAL,
+    id_country int DEFAULT 0,
+    code varchar(10) default '' NOT NULL,
+    value varchar(255) default '' NOT NULL,
+    is_attached SMALLINT DEFAULT 0,
+    date_validity_start DATE default '1900-01-01',
+    date_validity_end DATE default '2999-12-31',
+    deprecated SMALLINT DEFAULT 0,
+    date_update DATE default '1943-01-01',
+    author varchar(255) default '' NOT NULL,
+    status varchar(50) DEFAULT '' NOT NULL,
+    PRIMARY KEY (id_country_history, id_country)
+);
+
+ALTER TABLE geocodes_country_changes
+    ADD CONSTRAINT fk_id_country FOREIGN KEY (id_country) REFERENCES geocodes_country (id_country);
